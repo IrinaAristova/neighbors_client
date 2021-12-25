5ebbf5a774e5219d2a4b1b856501273c0d8c05dd
AristovaIrina <43518375+IrinaAristova@users.noreply.github.com>
2021-12-25 20:02:23 +0200

    Update index.js

--- Desktop/neighbors/client/src/http/index.js
+++ Desktop/neighbors/client/src/http/index.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const $host = axios.create({
-    baseURL:"http://localhost:5000/",
+    baseURL:"https://neighborsfriends.herokuapp.com/",
 
 })
 
 const $authHost = axios.create({
-    baseURL:"http://localhost:5000/",
+    baseURL:"https://neighborsfriends.herokuapp.com/",
 })
 
 const authInterceptor = config =>{
